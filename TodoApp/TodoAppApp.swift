63022de059914f1fcb4196f896e7f64d44806e8d
emptist <jigme1968@gmail.com>
2025-06-04 07:22:50 +0800

    refactoring

--- TodoApp/TodoAppApp.swift
+++ TodoApp/TodoAppApp.swift
@@ -10,3 +10,26 @@ struct TodoAppApp: App {
         .modelContainer(for: TaskItem.self) // Setup SwiftData container for TaskItem
     }
 }
+
+// for reference
+struct TodoAppApp_by_xcode: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            TaskItem.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
